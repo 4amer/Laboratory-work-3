dccf4e7666249ee1382d44bb3661ebb2bc1e6136
4amer <ggggamer23080032@gmail.com>
2023-04-29 22:39:52 +0300

    theerd commit

--- src/main/java/com/example/lb3/FrameLayout.java
+++ src/main/java/com/example/lb3/FrameLayout.java
@@ -2,13 +2,50 @@ package com.example.lb3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.material.snackbar.BaseTransientBottomBar;
+import com.google.android.material.snackbar.Snackbar;
 
 public class FrameLayout extends AppCompatActivity {
+
+    FrameLayout FrameLayout2;
+
+    int counter = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_frame_layout);
+
+        FrameLayout2 = findViewById(R.id.FrameLayout1);
+    }
+
+    public void Message(View v){
+        counter += 1;
+        Snackbar mySnackbar = Snackbar.make(FrameLayout2, "Кнопка была нажата "+counter+" раз", BaseTransientBottomBar.LENGTH_LONG);
+        mySnackbar.show();
+    }
+    public void Grid(View view){
+        Intent intent = new Intent(this, GridLayout.class);
+        startActivity(intent);
+    }
+
+    public void Register(View view){
+        Intent intent = new Intent(this, RegisterForm.class);
+        startActivity(intent);
+    }
+
+    public void Relative(View view) {
+        Intent intent = new Intent(this, RelativeLayout.class);
+        startActivity(intent);
+    }
+
+    public void Table(View view){
+        Intent intent = new Intent(this, TableLayout.class);
+        startActivity(intent);
     }
 }
--- src/main/java/com/example/lb3/MainActivity.java
+++ src/main/java/com/example/lb3/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.lb3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void Frame(View view){
+        Intent intent = new Intent(this, FrameLayout.class);
+        startActivity(intent);
+    }
+
+    public void Images(View view){
+        Intent intent = new Intent(this, ConstarintLayout.class);
+        startActivity(intent);
+    }
+
+    public void SignIn(View view){
+        Intent intent = new Intent(this, EnterAndRegisterLayout.class);
+        startActivity(intent);
     }
 }
